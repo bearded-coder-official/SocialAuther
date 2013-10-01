490ebd22ad29a2aec00e8bef364311b67bdc17af
Andrey Izman <cyborgcms@gmail.com>
2013-10-01 16:44:53 +0400

    Change Yandex adapter according to the new work logic

--- src/SocialAuther/Adapter/Yandex.php
+++ src/SocialAuther/Adapter/Yandex.php
@@ -8,17 +8,29 @@ class Yandex extends AbstractAdapter
     {
         parent::__construct($config);
 
-        $this->socialFieldsMap = array(
-            'socialId'   => 'id',
+        $this->fieldsMap = array(
+            'id'         => 'id',
             'email'      => 'default_email',
             'name'       => 'real_name',
-            'socialPage' => 'link',
-            'avatar'     => 'picture',
-            'sex'        => 'sex',
-            'birthday'   => 'birthday'
+            'page'       => 'link',
+            'image'      => 'picture'
         );
 
         $this->provider = 'yandex';
+    }
+
+    /**
+     * Get user sex or null if it is not set
+     *
+     * @return string|null
+     */
+    public function getSex()
+    {
+        if (isset($this->response['sex']) && in_array($this->response['sex'], array('male', 'female'))) {
+            return $this->response['sex'];
+        }
+
+        return null;
     }
 
     /**
@@ -28,8 +40,6 @@ class Yandex extends AbstractAdapter
      */
     public function authenticate()
     {
-        $result = false;
-
         if (isset($_GET['code'])) {
             $params = array(
                 'grant_type' => 'authorization_code',
@@ -49,13 +59,20 @@ class Yandex extends AbstractAdapter
                 $userInfo = $this->get('https://login.yandex.ru/info', $params);
 
                 if (isset($userInfo['id'])) {
-                    $this->userInfo = $userInfo;
-                    $result = true;
+                    $this->parseUserData($userInfo);
+
+                    if (isset($this->response['birthday'])) {
+                        $birthDate = explode('.', $this->response['birthday']);
+                        $this->userInfo['birthDay']   = isset($birthDate[0]) ? $birthDate[0] : null;
+                        $this->userInfo['birthMonth'] = isset($birthDate[1]) ? $birthDate[1] : null;
+                        $this->userInfo['birthYear']  = isset($birthDate[2]) ? $birthDate[2] : null;
+                    }
+                    return true;
                 }
             }
         }
 
-        return $result;
+        return false;
     }
 
     /**
